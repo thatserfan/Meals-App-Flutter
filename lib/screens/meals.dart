0dc238459bda6e6b57386a685cdb38117ec599b7
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-17 15:09:32 +0330

    Control when meal list is empty

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -13,10 +13,40 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try select another category',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+        ],
       ),
     );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(
+          meals[index].title,
+        ),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: content);
   }
 }
