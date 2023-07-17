c0b676df2f31255dc4263547eada4e87d24b54e4
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-17 14:50:01 +0330

    Update data & Create meals screen

---
+++ lib/screens/meals.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    required this.title,
+    required this.meals,
+    super.key,
+  });
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
