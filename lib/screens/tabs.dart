154755218a28278c058904300c3eb6c0037d4273
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-22 20:55:29 +0330

    Read option filters data

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -47,10 +47,10 @@ class TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.pop(context);
     if (identifier == 'filters') {
-      Navigator.of(context).push(
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
           builder: (ctx) => const FiltersScreen(),
         ),
