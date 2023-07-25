ec582d6d728ad1d39547a99c479707bda625a26e
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-25 14:20:05 +0330

    Initialize animation controller

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,7 +5,7 @@ import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreeen extends StatelessWidget {
+class CategoriesScreeen extends StatefulWidget {
   const CategoriesScreeen({
     super.key,
     required this.availableMeals,
@@ -13,8 +13,34 @@ class CategoriesScreeen extends StatelessWidget {
 
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreeen> createState() => _CategoriesScreeenState();
+}
+
+class _CategoriesScreeenState extends State<CategoriesScreeen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectedCategory(BuildContext context, Category category) {
-    final filterMeal = availableMeals
+    final filterMeal = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
