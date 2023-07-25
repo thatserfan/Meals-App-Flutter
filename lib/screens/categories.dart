6d0e7586be4254ebe097d47e1143067058f72d06
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-25 14:51:57 +0330

    Add ease in and out for category animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -79,10 +79,13 @@ class _CategoriesScreeenState extends State<CategoriesScreeen>
         ],
       ),
       builder: (context, child) => SlideTransition(
-        position: _animationController.drive(
-          Tween(
-            begin: const Offset(0, 0.3),
-            end: const Offset(0, 0),
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
           ),
         ),
         child: child,
